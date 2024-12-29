6e208fe58114787d91c86daa0fc8960a49831e14
Aleksander <93529824+MowiaMiSushi@users.noreply.github.com>
2024-12-29 20:16:19 +0100

    Poprawa danych

--- app/kontakt/data.ts
+++ app/kontakt/data.ts
@@ -1,35 +1,49 @@
 export const contactData = {
   hero: {
     title: "Kontakt",
-    description: "Skontaktuj się z nami i dowiedz się więcej o naszych usługach",
-    images: ['/images/hero-bg_1.webp', '/images/hero-bg_2.webp', '/images/hero-bg_3.webp']
+    description: "Jesteśmy do Twojej dyspozycji. Skontaktuj się z nami w dogodny dla Ciebie sposób.",
+    images: ["/images/hero-bg_1.jpg", "/images/hero-bg_2.jpg", "/images/hero-bg_3.jpg"]
   },
   contactInfo: {
     address: {
-      street: "ul. Szelągowska 24",
+      street: "ul. Węglowa 9/11",
       city: "Poznań",
-      postalCode: "61-626",
-      additionalInfo: "Wjazd od ul. Wilczak",
-      mapsUrl: "https://goo.gl/maps/1Z9Z2Z2Z2Z2Z2Z2Z2",
+      postalCode: "60-122",
+      additionalInfo: "(Wjazd od ulicy Górniczej)",
+      mapsUrl: "https://www.google.com/maps/place/Motoporadnia/@52.3820632,16.8732338,17z/data=!4m6!3m5!1s0x6b3a30447fea247d:0xfebbfd65d6023f50!8m2!3d52.3816215!4d16.8719611!16s%2Fg%2F11rylcfqmv",
       coordinates: {
-        latitude: 52.382063679785764,
-        longitude: 16.869772376940437
+        latitude: 52.381703007621105,
+        longitude: 16.871871248563785
       }
     },
-    phone: "789 059 578",
+    phone: "+48 789 059 578",
     email: "kontakt@motoporadnia.pl",
     hours: {
-      weekdays: "Pon-Pt: 8:30 - 16:30",
-      weekend: "Sobota-Niedziela: Nieczynne"
-    },
-    socialMedia: {
-      facebook: "https://facebook.com/motoporadnia",
-      instagram: "https://instagram.com/motoporadnia",
-      youtube: "https://youtube.com/motoporadnia"
+      weekdays: "8:30 - 16:30",
+      weekend: "zamknięte"
     }
   },
+  social: {
+    platforms: [
+      {
+        name: "Facebook",
+        url: "https://facebook.com/motoporadnia",
+        icon: "faFacebook"
+      },
+      {
+        name: "Instagram",
+        url: "https://instagram.com/motoporadnia",
+        icon: "faInstagram"
+      },
+      {
+        name: "YouTube",
+        url: "https://youtube.com/@motoporadnia",
+        icon: "faYoutube"
+      }
+    ]
+  },
   seo: {
-    type: "LocalBusiness",
+    type: "MotorcycleRepair",
     name: "MotoPoradnia",
     priceRange: "$$"
   }
