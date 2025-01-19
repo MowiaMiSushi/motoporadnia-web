ed834876caec6587926757d92563f66b08b8804a
Aleksander <93529824+MowiaMiSushi@users.noreply.github.com>
2025-01-19 21:29:11 +0100

    EH

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -13,7 +13,7 @@ let clientPromise: Promise<MongoClient>;
 if (process.env.NODE_ENV === 'development') {
   // W trybie development używamy globalnej zmiennej, aby zachować połączenie
   // podczas hot-reloading
-  let globalWithMongo = global as typeof globalThis & {
+  const globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>;
   };
 
